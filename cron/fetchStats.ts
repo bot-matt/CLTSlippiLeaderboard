e0c8485fb07f4bd94f62f650f9d492f7a14d734c
Bot-Matt <mtgottlieb@gmail.com>
2023-05-17 22:16:45 -0400

    edited things

--- cron/fetchStats.ts
+++ cron/fetchStats.ts
@@ -52,19 +52,6 @@ async function main() {
   console.log('Wrote new data file and timestamp.');
   const rootDir = path.normalize(path.join(__dirname, '..'))
   console.log(rootDir)
-  // if no current git changes
-  const { stdout, stderr } = await execPromise(`git -C ${rootDir} status --porcelain`);
-  if(stdout || stderr) {
-    console.log('Pending git changes... aborting deploy');
-    return
-  }
-  console.log('Deploying.');
-  const { stdout: stdout2, stderr: stderr2 } = await execPromise(`npm run --prefix ${rootDir} deploy`);
-  console.log(stdout2);
-  if(stderr2) {
-    console.error(stderr2);
-  }
-  console.log('Deploy complete.');
 }
 
 main();
