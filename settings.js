26d87d7a41282354f811cdf0bdb600ec226b219a
Matt <57820480+bot-matt@users.noreply.github.com>
2023-05-19 12:09:08 -0400

    Update settings.js

--- settings.js
+++ settings.js
@@ -9,7 +9,7 @@ module.exports = {
 
   // add repo path for username.github.io/repoPath for react router to recognize paths,
   // use only when deployed to github.io, leave as null for deployments with custom domains
-  repoPath: 'null',
+  repoPath: 'https://bot-matt.github.io/CLTSlippiLeaderboard/',
 
   // Google sheets ID for player connect codes.
   // https://docs.google.com/spreadsheets/d/[YOUR ID]
